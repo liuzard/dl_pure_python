8d238c17c06f86e276b6c34474aa9deca53706ac
liuxingchen <liuxingchen@tanzhishuju.com>
2022-01-29 15:19:12 +0800

    交叉熵损失函数修改

--- cnn/ch04/cross_entropy.py
+++ cnn/ch04/cross_entropy.py
@@ -17,13 +17,26 @@ def cross_entropy_oh(y_true, y_pred):
     return -np.sum(y_true * np.log(y_pred + delta)) / y_pred.shape[0]
 
 
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    # 监督数据是one-hot-vector的情况下，转换为正确解标签的索引
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
 def cross_entropy(y_true, y_pred):
     """交叉熵，批量计算，true标签为数值id形式下"""
     delta = 1e-7
     if y_pred.ndim == 1:
         y_true = y_true.reshape(1, y_true.size)
         y_pred = y_pred.reshape(1, y_pred.size)
-    print(y_pred[:, y_true])
+    # print(y_pred[:, y_true])
     return -np.sum(np.log(y_pred[np.arange(y_true.size), y_true] + delta)) / y_true.shape[0]
 
 
